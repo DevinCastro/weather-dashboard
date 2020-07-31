05aee397abe37e562341830bf600dba23a256503
Devin Castro <dcrocker21@gmail.com>
2020-07-31 01:28:08 -0700

    Got a solid template of html elemnts to render

---
+++ assets/app.js
@@ -0,0 +1,67 @@
+
+document.getElementById('search').addEventListener('click', event => {
+  event.preventDefault()
+
+  let city = document.getElementById('city').value
+
+  axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=79e391b4652051f3f627b227f5ffcac5
+`)
+    .then(res => {
+      console.log(res.data)
+
+      document.getElementById('weather').innerHTML = `
+      <div class="card">
+        <div class="card-body">
+          <h1>${res.data.name}</h1>
+          <h2>Weather: ${res.data.weather[0].description}</h2>
+          <h3>Temperature: ${res.data.main.temp}</h3>
+          <h3>Humidity: ${res.data.main.humidity}</h3>
+          <h3>Wind Speed: ${res.data.wind.speed}</h3>
+        </div>
+      </div>
+        
+        `
+    })
+    .catch(err => { console.log(err) })
+
+
+  // now forecast
+
+  axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=79e391b4652051f3f627b227f5ffcac5`)
+    .then(res => {
+      console.log(res)
+      let forecast = res.data.list
+
+      for (i = 6; i < forecast.length; i += 8) {
+
+        console.log(forecast[i])
+        let forecastElem = document.createElement('div')
+        forecastElem.className = 'col-2'
+        forecastElem.innerHTML = `
+        <div class="card">
+          <div class="card-body">
+            <h1>${forecast[i].dt_txt}</h1>
+            <h2>Weather: ${forecast[i].weather[0].description}</h2>
+            <h3>Temperature: ${forecast[i].main.temp}</h3>
+            <h3>Humidity: ${forecast[i].main.humidity}</h3>
+            <h3>Wind Speed: ${forecast[i].wind.speed}</h3>
+          </div>
+        </div>
+
+        `
+        document.getElementById('forecast').append(forecastElem)
+      }
+    })
+    .catch(err => { console.log(err) })
+
+
+
+
+
+
+
+
+
+
+
+})
